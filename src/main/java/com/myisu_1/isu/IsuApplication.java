6094ee54bb49ee2bca9a098fc481d27ed12af012
DIMON <user357@rambler.ru>
2022-05-24 08:52:37 +0600

    Added index

---
+++ src/main/java/com/myisu_1/isu/IsuApplication.java
@@ -0,0 +1,13 @@
+package com.myisu_1.isu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myisu_1/isu/IsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myisu_1.isu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
