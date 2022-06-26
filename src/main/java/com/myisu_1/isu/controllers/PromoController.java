a237ddad2104c981a62f2d863f1d6cf72f54453c
DIMON <user357@rambler.ru>
2022-06-26 11:58:16 +0600

    Add botstp555

--- src/main/java/com/myisu_1/isu/controllers/PromoController.java
+++ src/main/java/com/myisu_1/isu/controllers/PromoController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
@@ -32,7 +31,7 @@ public class PromoController {
     HashSet<String> phon;
     HashSet<String> phonGb;
     List<Distinct> one;
-    
+
     @Autowired
     private PhoneRepositoriy phoneRepositoriy;
     @Autowired
@@ -42,24 +41,26 @@ public class PromoController {
     @Autowired
     private MarwelPromoRepositoriy marwelPromoRepositoriy;
 
-   @GetMapping("/promo")
-    public String Brend (Model model) {
-       phone = new ArrayList<>();
-       promoMarwel = new ArrayList<>();
-       model.addAttribute("phone", phone);
-       phones = (List<Phone_Smart>) phoneRepositoriy.findAll();
-       phon = new HashSet<>();
-        for(int i = 1;i< phones.size();i++){
-           phon.add(phones.get(i).getBrend());
-               }
-                model.addAttribute("phones", Sorting(phon));
-       all_promo = (List<price_promo>) promoRepositoriy.findAll(Sort.by(Sort.Direction.DESC, "startPromo"));
-       model.addAttribute("all_promo", all_promo);
-       model.addAttribute("current_promoMarwel", current_promoMarwel());
+    @GetMapping("/promo")
+    public String Brend(Model model) {
+        phone = new ArrayList<>();
+        promoMarwel = new ArrayList<>();
+        model.addAttribute("phone", phone);
+        phones = (List<Phone_Smart>) phoneRepositoriy.findAll();
+        phon = new HashSet<>();
+        for (int i = 1; i < phones.size(); i++) {
+            phon.add(phones.get(i).getBrend());
+        }
+        model.addAttribute("phones", Sorting(phon));
+        all_promo = (List<price_promo>) promoRepositoriy.findAll(Sort.by(Sort.Direction.DESC, "startPromo"));
+        model.addAttribute("all_promo", all_promo);
+        model.addAttribute("current_promoMarwel", current_promoMarwel());
+        model.addAttribute("current_promo", current_promo());
+        model.addAttribute("startpromo", startpromo());
+        model.addAttribute("endpromo", endpromo());
 
         return "promo";
     }
-
 
 
     @ResponseBody
@@ -67,31 +68,31 @@ public class PromoController {
     public String loadBrend(@PathVariable("brend") String brend) {
         phonGb = new HashSet<>();
         phonGb.add("----");
-        for(int i = 1;i< phones.size();i++){
-            if (brend.equals((phones.get(i).getBrend()))){
+        for (int i = 0; i < phones.size(); i++) {
+            if (brend.equals((phones.get(i).getBrend()))) {
                 phonGb.add(phones.get(i).getModel_GB());
             }
         }
-            Gson gson = new Gson();
+        Gson gson = new Gson();
         return gson.toJson(Sorting(phonGb));
     }
 
     @ResponseBody
     @RequestMapping(value = "mode/{mode}", method = RequestMethod.GET)
     public String loadModel(@PathVariable("mode") String mode) {
-       String name = null;
+        String name = null;
         String price_phone = null;
-        for(int i = 1;i< phones.size();i++){
-            if (mode.replaceAll("_", "/").equals((phones.get(i).getModel_GB()))){
-                     name = phones.get(i).getModel();
+        for (int i = 0; i < phones.size(); i++) {
+            if (mode.replaceAll("_", "/").equals((phones.get(i).getModel_GB()))) {
+                name = phones.get(i).getModel();
             }
         }
         price = (List<retail_price>) priceRepositoriy.findAll();
 
-        for(int i = 1;i< price.size();i++){
-                if (name.equals((price.get(i).getName()))){
+        for (int i = 0; i < price.size(); i++) {
+            if (name.equals((price.get(i).getName()))) {
                 price_phone = price.get(i).getPrice();
-                }
+            }
         }
         return price_phone;
     }
@@ -110,16 +111,19 @@ public class PromoController {
                             @RequestParam String Merlion,
                             Model model) {
 
-        if(IDupdate!=0) {
+        if (IDupdate != 0) {
 
-            price_promo pricePromo = new price_promo(IDupdate,brend, models, price, Promo_price, start_date, end_date, Marwel, TFN, ВВП, Merlion);
+            price_promo pricePromo = new price_promo(IDupdate, brend, models, price, Promo_price, start_date, end_date, Marwel, TFN, ВВП, Merlion);
             promoRepositoriy.save(pricePromo);
             model.addAttribute("phones", Sorting(phon));
             all_promo = (List<price_promo>) promoRepositoriy.findAll(Sort.by(Sort.Direction.DESC, "startPromo"));
             model.addAttribute("all_promo", all_promo);
             model.addAttribute("current_promoMarwel", current_promoMarwel());
+            model.addAttribute("current_promo", current_promo());
+            model.addAttribute("startpromo", startpromo());
+            model.addAttribute("endpromo", endpromo());
             return "promo";
-        }else{
+        } else {
             price_promo pricePromo = new price_promo(brend, models, price, Promo_price, start_date, end_date, Marwel, TFN, ВВП, Merlion);
             promoRepositoriy.save(pricePromo);
 
@@ -129,26 +133,36 @@ public class PromoController {
         all_promo = (List<price_promo>) promoRepositoriy.findAll(Sort.by(Sort.Direction.DESC, "startPromo"));
         model.addAttribute("all_promo", all_promo);
         model.addAttribute("current_promoMarwel", current_promoMarwel());
+        model.addAttribute("current_promo", current_promo());
+        model.addAttribute("startpromo", startpromo());
+        model.addAttribute("endpromo", endpromo());
         return "promo";
     }
 
     @PostMapping("/search")
-    public String search(   @RequestParam String searchModels,
-                            @RequestParam String startSearch,
-                            @RequestParam String endSearch,
-                            Model model) {
+    public String search(@RequestParam String searchModels,
+                         @RequestParam String startSearch,
+                         @RequestParam String endSearch,
+                         Model model) {
 
 
         model.addAttribute("phones", Sorting(phon));
-        model.addAttribute("all_promo", search(searchModels,startSearch,endSearch));
+        model.addAttribute("all_promo", search(searchModels, startSearch, endSearch));
         model.addAttribute("current_promoMarwel", current_promoMarwel());
+        model.addAttribute("current_promo", current_promo());
+        model.addAttribute("startpromo", startpromo());
+        model.addAttribute("endpromo", endpromo());
         return "promo";
     }
+
     @PostMapping("/reset")
-    public String reset( Model model) {
+    public String reset(Model model) {
         model.addAttribute("phones", Sorting(phon));
         model.addAttribute("all_promo", all_promo);
         model.addAttribute("current_promoMarwel", current_promoMarwel());
+        model.addAttribute("current_promo", current_promo());
+        model.addAttribute("startpromo", startpromo());
+        model.addAttribute("endpromo", endpromo());
         return "promo";
     }
 
@@ -158,10 +172,11 @@ public class PromoController {
 
         return promoRepositoriy.findById(id);
     }
+
     @PostMapping("/delet")
     public String delet(@RequestParam int ID,
 
-                            Model model) {
+                        Model model) {
 
         System.out.println(ID);
         promoRepositoriy.deleteById(ID);
@@ -169,9 +184,13 @@ public class PromoController {
         all_promo = (List<price_promo>) promoRepositoriy.findAll(Sort.by(Sort.Direction.DESC, "startPromo"));
         model.addAttribute("all_promo", all_promo);
         model.addAttribute("current_promoMarwel", current_promoMarwel());
+        model.addAttribute("current_promo", current_promo());
+        model.addAttribute("startpromo", startpromo());
+        model.addAttribute("endpromo", endpromo());
         return "promo";
     }
-    public List<Distinct> Sorting (HashSet<String> phon){
+
+    public List<Distinct> Sorting(HashSet<String> phon) {
         distinct = new Distinct();
         one = new ArrayList<>();
         TreeSet myTreeSet = new TreeSet();
@@ -182,26 +201,26 @@ public class PromoController {
         return one;
     }
 
-    public List<price_promo> search (String searchModels,String startSearch,String endSearch){
-    search = new ArrayList<>();
-    for ( int i =1; i<all_promo.size();i++){
-    if (all_promo.get(i).getBrend().equals(searchModels)){
-        search.add(new price_promo(
-                all_promo.get(i).getId(),
-                all_promo.get(i).getBrend(),
-                all_promo.get(i).getModels(),
-                all_promo.get(i).getPrice(),
-                all_promo.get(i).getPrice_promo(),
-                all_promo.get(i).getStartPromo(),
-                all_promo.get(i).getEndPromo(),
-                all_promo.get(i).getMarwel(),
-                all_promo.get(i).getTfn(),
-                all_promo.get(i).getVvp(),
-                all_promo.get(i).getMerlion()));
+    public List<price_promo> search(String searchModels, String startSearch, String endSearch) {
+        search = new ArrayList<>();
+        for (int i = 0; i < all_promo.size(); i++) {
+            if (all_promo.get(i).getBrend().equals(searchModels)) {
+                search.add(new price_promo(
+                        all_promo.get(i).getId(),
+                        all_promo.get(i).getBrend(),
+                        all_promo.get(i).getModels(),
+                        all_promo.get(i).getPrice(),
+                        all_promo.get(i).getPrice_promo(),
+                        all_promo.get(i).getStartPromo(),
+                        all_promo.get(i).getEndPromo(),
+                        all_promo.get(i).getMarwel(),
+                        all_promo.get(i).getTfn(),
+                        all_promo.get(i).getVvp(),
+                        all_promo.get(i).getMerlion()));
 
-    //    System.out.println(searchModels + "-"+ startSearch +"-"+ endSearch);
-    }
-}
+                //    System.out.println(searchModels + "-"+ startSearch +"-"+ endSearch);
+            }
+        }
 
         return search;
     }
@@ -210,30 +229,110 @@ public class PromoController {
 
         List<MarvelPromo> current_promoMarwel = new ArrayList<>();
 
-        Date endDate = new Date();
+
 
         promoMarwel = (List<MarvelPromo>) marwelPromoRepositoriy.findAll();
-for(int i = 1;i<promoMarwel.size();i++){
-    if(promoMarwel.get(i).getStartPromo().getTime()<=endDate.getTime() && promoMarwel.get(i).getEndPromo().getTime() >= endDate.getTime()){
-        MarvelPromo marvelPromo = new MarvelPromo();
-        current_promoMarwel.add(new MarvelPromo(
-                promoMarwel.get(i).getId(),
-                promoMarwel.get(i).getPromoCode(),
-                promoMarwel.get(i).getStartPromo(),
-                promoMarwel.get(i).getEndPromo(),
-                promoMarwel.get(i).getArticleNumber(),
-                promoMarwel.get(i).getVision(),
-                promoMarwel.get(i).getNewVision(),
-                promoMarwel.get(i).getDiscount(),
-                promoMarwel.get(i).getCompensation(),
-                promoMarwel.get(i).getCollecting(),
-                promoMarwel.get(i).getStatus()
+        for (int i = 0; i < promoMarwel.size(); i++) {
+            if (promoMarwel.get(i).getStartPromo().getTime() <= current_date().getTime() && promoMarwel.get(i).getEndPromo().getTime() >= current_date().getTime()) {
+
+                current_promoMarwel.add(new MarvelPromo(
+                        promoMarwel.get(i).getId(),
+                        promoMarwel.get(i).getPromoCode(),
+                        promoMarwel.get(i).getStartPromo(),
+                        promoMarwel.get(i).getEndPromo(),
+                        promoMarwel.get(i).getArticleNumber(),
+                        promoMarwel.get(i).getVision(),
+                        promoMarwel.get(i).getNewVision(),
+                        promoMarwel.get(i).getDiscount(),
+                        promoMarwel.get(i).getCompensation(),
+                        promoMarwel.get(i).getCollecting(),
+                        promoMarwel.get(i).getStatus()
                 ));
 
-    }
-}
+            }
+        }
 
         return current_promoMarwel;
     }
 
+    private List<price_promo> current_promo() {
+        List<price_promo> current_promo = new ArrayList<>();
+
+        for (int i = 0; i < all_promo.size(); i++) {
+            if (all_promo.get(i).getStartPromo().getTime() <= current_date().getTime() && all_promo.get(i).getEndPromo().getTime() >= current_date().getTime()) {
+                current_promo.add(new price_promo(
+                        all_promo.get(i).getId(),
+                        all_promo.get(i).getBrend(),
+                        all_promo.get(i).getModels(),
+                        all_promo.get(i).getPrice(),
+                        all_promo.get(i).getPrice_promo(),
+                        all_promo.get(i).getStartPromo(),
+                        all_promo.get(i).getEndPromo(),
+                        all_promo.get(i).getMarwel(),
+                        all_promo.get(i).getTfn(),
+                        all_promo.get(i).getVvp(),
+                        all_promo.get(i).getMerlion()
+                ));
+
+        }
+        }
+        return current_promo;
+    }
+    private List<price_promo> startpromo() {
+
+        List<price_promo> startpromo = new ArrayList<>();
+
+        for (int i = 0; i < all_promo.size(); i++) {
+            if (all_promo.get(i).getStartPromo().getTime() == current_date().getTime()) {
+
+                startpromo.add(new price_promo(
+                        all_promo.get(i).getId(),
+                        all_promo.get(i).getBrend(),
+                        all_promo.get(i).getModels(),
+                        all_promo.get(i).getPrice(),
+                        all_promo.get(i).getPrice_promo(),
+                        all_promo.get(i).getStartPromo(),
+                        all_promo.get(i).getEndPromo(),
+                        all_promo.get(i).getMarwel(),
+                        all_promo.get(i).getTfn(),
+                        all_promo.get(i).getVvp(),
+                        all_promo.get(i).getMerlion()
+                ));
+        }
+
+        }
+        return startpromo;
+    }
+    private List<price_promo> endpromo() {
+        List<price_promo> endpromo = new ArrayList<>();
+
+        for (int i = 0; i < all_promo.size(); i++) {
+            if (all_promo.get(i).getEndPromo().getTime() == current_date().getTime()) {
+                endpromo.add(new price_promo(
+                        all_promo.get(i).getId(),
+                        all_promo.get(i).getBrend(),
+                        all_promo.get(i).getModels(),
+                        all_promo.get(i).getPrice(),
+                        all_promo.get(i).getPrice_promo(),
+                        all_promo.get(i).getStartPromo(),
+                        all_promo.get(i).getEndPromo(),
+                        all_promo.get(i).getMarwel(),
+                        all_promo.get(i).getTfn(),
+                        all_promo.get(i).getVvp(),
+                        all_promo.get(i).getMerlion()
+                ));
+
+            }
+        }
+        return endpromo;
+    }
+    Date current_date(){
+        int den, mes, god;
+        final Calendar c = Calendar.getInstance();
+        den = c.get(Calendar.DAY_OF_MONTH);
+        mes = c.get(Calendar.MONTH);
+        god = c.get(Calendar.YEAR);
+        Date endDate = new Date(god-1900,mes,den);
+        return endDate;
+    }
 }
