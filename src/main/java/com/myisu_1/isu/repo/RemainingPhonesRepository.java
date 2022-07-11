53ff1f54ec4a2160d65104b88223fe748db3fec7
DIMON <user357@rambler.ru>
2022-07-11 23:35:36 +0600

    RemainingPhonesRepository

---
+++ src/main/java/com/myisu_1/isu/repo/RemainingPhonesRepository.java
@@ -0,0 +1,8 @@
+package com.myisu_1.isu.repo;
+
+import com.myisu_1.isu.models.RemainingPhones;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RemainingPhonesRepository extends CrudRepository<RemainingPhones, Integer> {
+}
