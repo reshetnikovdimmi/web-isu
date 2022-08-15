58c64ac1c3aa38b34c89125af15c62da03ba4a29
DIMON <user357@rambler.ru>
2022-08-15 23:23:14 +0600

    add search sim

--- src/main/java/com/myisu_1/isu/controllers/SimController.java
+++ src/main/java/com/myisu_1/isu/controllers/SimController.java
@@ -2,10 +2,14 @@ package com.myisu_1.isu.controllers;
 
 
 import com.myisu_1.isu.models.Distinct;
+import com.myisu_1.isu.models.SIM.RemanisSim;
 import com.myisu_1.isu.models.SIM.RemanisSimRarus;
+import com.myisu_1.isu.models.SIM.SimAndRtkTable;
 import com.myisu_1.isu.models.authorization_tt;
 import com.myisu_1.isu.repo.PostRepositoriy;
 import com.myisu_1.isu.repo.RemanisSimRarusrepository;
+import com.myisu_1.isu.repo.RemanisSimRepository;
+import com.myisu_1.isu.repo.SimAndRtkTableRepositoriy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,24 +23,47 @@ import java.util.List;
 @Controller
 public class SimController {
 
-    List<authorization_tt> shopall;
-    List<Distinct> one;
+
     @Autowired
-    private PostRepositoriy shop;
+    private PostRepositoriy authorization_shop;
     @Autowired
-    private RemanisSimRarusrepository remanisSimRarusrepository;
+    private RemanisSimRepository remanisSimrepository;
+    @Autowired
+    private SimAndRtkTableRepositoriy simAndRtkTableRepositoriy;
 
 
     @GetMapping("/SIM")
     public String sim(Model model) {
-        model.addAttribute("shop", shop.findAll());
+        model.addAttribute("shop", authorization_shop.findAll());
         return "SIM";
     }
 
     @ResponseBody
     @RequestMapping(value = "updateShops/{Shop}", method = RequestMethod.GET)
-    public Iterable<RemanisSimRarus> update(@PathVariable("Shop") String shop) {
-        return remanisSimRarusrepository.findAll();
+    public Iterable<RemanisSim> update(@PathVariable("Shop") String shop) {
+
+        List<RemanisSim> remanisSims = new ArrayList<>();
+        List<RemanisSim> remanisSimList = (List<RemanisSim>)remanisSimrepository.findAll();
+        List<authorization_tt> authorization_ttList = (List<authorization_tt>) authorization_shop.findAll();
+        List<SimAndRtkTable> simAndRtkTables = simAndRtkTableRepositoriy.findAll();
+
+        for(int i =0;i<authorization_ttList.size();i++){
+            System.out.println(authorization_ttList.get(i).getName());
+            if(shop.equals(authorization_ttList.get(i).getName())){
+                System.out.println(shop);
+                for (int j =0;j < remanisSimList.size();j++){
+                    if (authorization_ttList.get(i).getShopIskra().equals(remanisSimList.get(j).getShop())){
+
+                        remanisSims.add(new RemanisSim(remanisSimList.get(j).getId(),
+                                remanisSimList.get(j).getShop(),
+                                remanisSimList.get(j).getNameSimAndModem(),
+                                remanisSimList.get(j).getRemainsSimModem()));
+                    }
+                }
+            }
+        }
+
+        return remanisSims;
     }
 
 
--- src/main/resources/static/JS/SIM.js
+++ src/main/resources/static/JS/SIM.js
@@ -24,25 +24,28 @@ $(document).ready(function(){
                             var elem = document.querySelector('#table_t2m');
                             var elem1 = document.querySelector('#table_t3m');
                             elem1.parentNode.removeChild(elem1);
-                                                        createTable(elem,3,15,sim);
+                                                        createTable(elem,3,sim.length,sim);
 
                           });
                                 function createTable(parent,cols,rows,shops){
                                  var table = document.createElement(`table-borderless-sm`);
+                                 table.id = 'table_t3m';
                                     let thead = document.createElement('thead');
                                     let tbody = document.createElement('tbody');
-let row_1 = document.createElement('tr');
-let heading_1 = document.createElement('th');
-heading_1.innerHTML = "Sr. No.";
-let heading_2 = document.createElement('th');
-heading_2.innerHTML = "Name";
-let heading_3 = document.createElement('th');
-heading_3.innerHTML = "Company";
 
-row_1.appendChild(heading_1);
-row_1.appendChild(heading_2);
-row_1.appendChild(heading_3);
-thead.appendChild(row_1);
+                                        let row_1 = document.createElement('tr');
+                                        let heading_1 = document.createElement('th');
+                                        heading_1.innerHTML = "Sr. No.";
+                                        let heading_2 = document.createElement('th');
+                                        heading_2.innerHTML = "Name";
+                                        let heading_3 = document.createElement('th');
+                                        heading_3.innerHTML = "Company";
+
+                                        row_1.appendChild(heading_1);
+                                        row_1.appendChild(heading_2);
+                                        row_1.appendChild(heading_3);
+                                        thead.appendChild(row_1);
+
                                     table.appendChild(thead);
 
 
@@ -52,7 +55,15 @@ thead.appendChild(row_1);
 
                                             for (var j = 0; j < cols; j++) {
                                                 var td = document.createElement('td');
-                                               td.innerHTML = shops[j].id;
+                                               if(j==0){
+
+                                               td.innerHTML = shops[i].nameSimAndModem;
+                                               } else if(j==1){
+                                               td.innerHTML = shops[i].remainsSimModem;
+                                               }else{
+                                               td.innerHTML = shops[i].shop;
+                                               }
+
                                                 tr.appendChild(td);
 
                                             }
