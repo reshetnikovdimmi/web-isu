f76a7039ce6a5ee5dfaefe2512a851eb76272ae9
DIMON <user357@rambler.ru>
2022-08-16 23:09:56 +0600

    add input in sim

--- src/main/resources/static/JS/SIM.js
+++ src/main/resources/static/JS/SIM.js
@@ -24,14 +24,16 @@ $(document).ready(function(){
                             var elem = document.querySelector('#table_t2m');
                             var elem1 = document.querySelector('#table_t3m');
                             elem1.parentNode.removeChild(elem1);
-                                                        createTable(elem,3,sim.length,sim);
+                                                        createTable(elem,4,sim.length,sim);
 
                           });
                                 function createTable(parent,cols,rows,shops){
-                                 var table = document.createElement(`table-borderless-sm`);
+                                 var table = document.createElement(`table`);
                                  table.id = 'table_t3m';
+                                 table.classList.add("table-borderless-sm");
+
                                     let thead = document.createElement('thead');
-                                    let tbody = document.createElement('tbody');
+
 
                                         let row_1 = document.createElement('tr');
                                         let heading_1 = document.createElement('th');
@@ -40,16 +42,21 @@ $(document).ready(function(){
                                         heading_2.innerHTML = "Name";
                                         let heading_3 = document.createElement('th');
                                         heading_3.innerHTML = "Company";
+                                        let heading_4 = document.createElement('th');
+                                        heading_4.innerHTML = "план";
 
                                         row_1.appendChild(heading_1);
                                         row_1.appendChild(heading_2);
                                         row_1.appendChild(heading_3);
+                                        row_1.appendChild(heading_4);
                                         thead.appendChild(row_1);
 
                                     table.appendChild(thead);
 
-
+                                    let tbody = document.createElement('tbody');
                                   console.log(cols);
+
+
                                         for (var i = 0; i < rows; i++) {
                                             var tr = document.createElement('tr');
 
@@ -60,8 +67,15 @@ $(document).ready(function(){
                                                td.innerHTML = shops[i].nameSimAndModem;
                                                } else if(j==1){
                                                td.innerHTML = shops[i].remainsSimModem;
-                                               }else{
+                                               }else if(j==2){
                                                td.innerHTML = shops[i].shop;
+                                               }else{
+                                                var input = document.createElement("input");
+
+                                                    input.type = 'text';
+
+                                                    input.setAttribute("class", "JSON");
+                                               td.appendChild(input);
                                                }
 
                                                 tr.appendChild(td);
