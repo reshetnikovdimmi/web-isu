2026b57f83a9e87372e74a8279d0bd35dcb34e7c
DIMON <user357@rambler.ru>
2022-08-25 00:24:04 +0600

    added sim to the distribution page

--- src/main/java/com/myisu_1/isu/controllers/SimDistributionController.java
+++ src/main/java/com/myisu_1/isu/controllers/SimDistributionController.java
@@ -1,16 +1,25 @@
 package com.myisu_1.isu.controllers;
 
+import com.myisu_1.isu.repo.PostRepositoriy;
+import com.myisu_1.isu.repo.SimAndRtkTableRepositoriy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SimDistributionController {
+    @Autowired
+    private PostRepositoriy authorization_shop;
+    @Autowired
+    private SimAndRtkTableRepositoriy simAndRtkTableRepositoriy;
+
     @GetMapping("/SimDistribution")
     public String SimDistribution (Model model) {
 
 
-
+        model.addAttribute("shop", authorization_shop.findAll());
+        model.addAttribute("simAndRtkTable", simAndRtkTableRepositoriy.findAll());
 
         return "SimDistribution";
     }
