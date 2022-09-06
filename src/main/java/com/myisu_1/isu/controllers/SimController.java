23032f76246040abf9664b0cd087c5953bd32978
DIMON <user357@rambler.ru>
2022-09-07 00:05:56 +0600

    added other sim

--- src/main/java/com/myisu_1/isu/controllers/SimController.java
+++ src/main/java/com/myisu_1/isu/controllers/SimController.java
@@ -54,8 +54,10 @@ public class SimController {
     @RequestMapping(value = "updateShops/{Shop}/{t2}", method = RequestMethod.GET)
     public Iterable<SimSvod> update(@PathVariable("Shop") String shop,@PathVariable("t2") String t2) {
 
+
         simSvodList = new ArrayList<>();
         simSvodList = (List<SimSvod>) simList.parse3(shop,t2);
+
         return simList.parse3(shop,t2);
     }
 
--- src/main/resources/static/JS/SimDistribution.js
+++ src/main/resources/static/JS/SimDistribution.js
@@ -4,7 +4,180 @@ const requestURL = '/simos'
     $(".nav-tabs a").click(function(){
         $(this).tab('show');
     });
+    $('.table_mts_sim .btn').on('click', function(event){
+              event.preventDefault();
+              var href = $(this).attr("href");
+                   $.get(href, function(SIM, status){
+                        var sim = SIM;
+                        console.log(sim);
+                        var shop = SIM[0].nameSim;
+                        document.querySelector('#Shopmts').innerHTML = shop;
+                        var elem = document.querySelector('#table_mts_sim2');
+                        var elem1 = document.querySelector('#table_mts_sim3');
+                        elem1.parentNode.removeChild(elem1);
+                        createTableSimMTS(elem,3,sim.length,sim);
 
+                   });
+function createTableSimMTS(parent,cols,rows,shops){
+
+                                            var table = document.createElement(`table`);
+                                            table.id = 'table_mts_sim3';
+                                            table.classList.add("table-borderless");
+
+                                               let thead = document.createElement('thead');
+
+
+                                                   let row_1 = document.createElement('tr');
+                                                   let heading_1 = document.createElement('th');
+                                                   heading_1.innerHTML = "Name SIM";
+                                                   let heading_2 = document.createElement('th');
+                                                   heading_2.innerHTML = "Кол-во";
+
+
+                                                   row_1.appendChild(heading_1);
+                                                   row_1.appendChild(heading_2);
+
+
+                                                   thead.appendChild(row_1);
+
+                                               table.appendChild(thead);
+
+                                               let tbody = document.createElement('tbody');
+
+                                                   for (var i = 0; i < rows; i++) {
+                                                       var tr = document.createElement('tr');
+
+                                                       for (var j = 0; j < cols; j++) {
+                                                           var td = document.createElement('td');
+                                                          if(j==0){
+                                                          td.innerHTML = shops[i].shop;
+                                                          } else if(j==1){
+                                                          td.innerHTML = shops[i].toOrder;
+                                                          }else{
+                                                          td.innerHTML = shops[i].distribution;
+                                                          }
+
+                                                           tr.appendChild(td);
+
+                                                       }
+                                                       tbody.appendChild(tr);
+                                                   }
+                                                   table.appendChild(tbody);
+                                                   parent.appendChild(table);
+
+                                                   var tds = document.querySelectorAll('table.table-borderless td');
+                                                     for(var i =4;i<tds.length;i+=6){
+                                                         tds[i].addEventListener('click', function func(){
+
+                                                               var input = document.createElement('input');
+
+                                                               input.value = this.innerHTML;
+                                                               this.innerHTML = '';
+                                                               this.appendChild(input);
+
+                                                               var td = this;
+                                                               input.addEventListener('blur', function(){
+                                                                   td.innerHTML = this.value;
+                                                                tab =  this.value;
+                                                                   td.addEventListener('click', func);
+
+                                                               });
+
+
+                                                               this.removeEventListener('click', func)
+                                                         });
+                                                     }
+
+                                             }
+
+                   });
+
+$('.table_mts .btn').on('click', function(event){
+          event.preventDefault();
+          var href = $(this).attr("href");
+               $.get(href, function(SIM, status){
+                    var sim = SIM;
+                    console.log(sim);
+                    var shop = SIM[0].shop;
+                    document.querySelector('#ShopSimDistribution_mts').innerHTML = shop;
+                    var elem = document.querySelector('#table_mts2');
+                    var elem1 = document.querySelector('#table_mts3');
+                    elem1.parentNode.removeChild(elem1);
+                    createTableMTS(elem,3,sim.length,sim);
+
+               });
+function createTableMTS(parent,cols,rows,shops){
+                                                var table = document.createElement(`table`);
+                                                table.id = 'table_mts3';
+                                                table.classList.add("table-borderless");
+
+                                                   let thead = document.createElement('thead');
+
+
+                                                       let row_1 = document.createElement('tr');
+                                                       let heading_1 = document.createElement('th');
+                                                       heading_1.innerHTML = "Name SIM";
+                                                       let heading_2 = document.createElement('th');
+                                                       heading_2.innerHTML = "Кол-во";
+
+
+                                                       row_1.appendChild(heading_1);
+                                                       row_1.appendChild(heading_2);
+
+
+                                                       thead.appendChild(row_1);
+
+                                                   table.appendChild(thead);
+
+                                                   let tbody = document.createElement('tbody');
+
+                                                       for (var i = 0; i < rows; i++) {
+                                                           var tr = document.createElement('tr');
+
+                                                           for (var j = 0; j < cols; j++) {
+                                                               var td = document.createElement('td');
+                                                              if(j==0){
+                                                              td.innerHTML = shops[i].nameSim;
+                                                              } else if(j==1){
+                                                              td.innerHTML = shops[i].toOrder;
+                                                              }else{
+                                                              td.innerHTML = shops[i].distribution;
+                                                              }
+
+                                                               tr.appendChild(td);
+
+                                                           }
+                                                           tbody.appendChild(tr);
+                                                       }
+                                                       table.appendChild(tbody);
+                                                       parent.appendChild(table);
+
+                                                       var tds = document.querySelectorAll('table.table-borderless td');
+                                                         for(var i =4;i<tds.length;i+=6){
+                                                             tds[i].addEventListener('click', function func(){
+
+                                                                   var input = document.createElement('input');
+
+                                                                   input.value = this.innerHTML;
+                                                                   this.innerHTML = '';
+                                                                   this.appendChild(input);
+
+                                                                   var td = this;
+                                                                   input.addEventListener('blur', function(){
+                                                                       td.innerHTML = this.value;
+                                                                    tab =  this.value;
+                                                                       td.addEventListener('click', func);
+
+                                                                   });
+
+
+                                                                   this.removeEventListener('click', func)
+                                                             });
+                                                         }
+
+                                                 }
+
+               });
      $('.table_t2 .btn').on('click', function(event){
           event.preventDefault();
           var href = $(this).attr("href");
@@ -19,6 +192,7 @@ const requestURL = '/simos'
                     createTableMono(elem,3,sim.length,sim);
 
                });
+
                function createTableMono(parent,cols,rows,shops){
                                                 var table = document.createElement(`table`);
                                                 table.id = 'table_t3';
