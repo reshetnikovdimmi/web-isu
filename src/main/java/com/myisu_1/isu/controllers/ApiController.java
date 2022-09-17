b6792341304a65d23490a80d9eecf6e052379250
DIMON <user357@rambler.ru>
2022-09-17 19:54:39 +0600

    add postData(log String)

--- src/main/java/com/myisu_1/isu/controllers/ApiController.java
+++ src/main/java/com/myisu_1/isu/controllers/ApiController.java
@@ -1,16 +1,12 @@
 package com.myisu_1.isu.controllers;
 
-import com.myisu_1.isu.models.SIM.RemanisSim;
 import com.myisu_1.isu.models.authorization_tt;
 import com.myisu_1.isu.repo.PostRepositoriy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -39,13 +35,13 @@ public class ApiController {
         return login;
     }
    @PostMapping(path = "/api/save")
-    private List<authorization_tt> simos(@RequestParam String login, @RequestParam String password) {
+    private String simos(@RequestParam String login, @RequestParam String password) {
         List<authorization_tt> log = new ArrayList<>();
         authorization_tt logins = new authorization_tt();
         logins.setLogin(login);
         log.add(logins);
         System.out.println("requestBody");
 
-        return log;
+        return "log";
     }
 }
