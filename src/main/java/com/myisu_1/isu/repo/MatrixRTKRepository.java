ff5b50323f9fa695c7aef8a2a9ad0ec9de4378b8
DIMON <user357@rambler.ru>
2022-10-13 09:22:53 +0600

    matrixrtk new

---
+++ src/main/java/com/myisu_1/isu/repo/MatrixRTKRepository.java
@@ -0,0 +1,7 @@
+package com.myisu_1.isu.repo;
+
+import com.myisu_1.isu.models.RTK.MatrixRTK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatrixRTKRepository extends JpaRepository<MatrixRTK, Integer> {
+}
