f8162962b3944476cbe2beadc8a940352231127a
DIMON <user357@rambler.ru>
2022-10-13 09:06:50 +0600

    0000

--- src/main/java/com/myisu_1/isu/repo/MatrixRTKRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myisu_1.isu.repo;
-
-
-import com.myisu_1.isu.models.RTK.MatrixRTK;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatrixRTKRepository extends JpaRepository<MatrixRTK, Integer> {
-}
