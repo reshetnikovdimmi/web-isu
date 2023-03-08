14b783d4fb0d85e6135e46a206bcd9ce7993a996
DIMON <user357@rambler.ru>
2023-03-08 20:20:28 +0600

    add RRD

--- src/main/java/com/myisu_1/isu/repo/RemainingPhonesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myisu_1.isu.repo;
-
-import com.myisu_1.isu.models.RemainingPhones;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RemainingPhonesRepository extends CrudRepository<RemainingPhones, Integer> {
-}
