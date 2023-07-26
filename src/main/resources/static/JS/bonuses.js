c8924853b79645c44cb1f784f89017850632645e
DIMON <user357@rambler.ru>
2023-07-26 23:54:42 +0600

    поправлена дата

--- src/main/resources/static/JS/bonuses.js
+++ src/main/resources/static/JS/bonuses.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     var month = ("0" + (firstDay.getMonth() + 1)).slice(-2);
     var today = firstDay.getFullYear() + "-" + (month) + "-" + (day);
     $("#start").val(today);
-    bonuses.startDate = firstDay;
+    bonuses.startDate = today;
     sendRequest('POST', '/loadBonusesAll', bonuses).then(data => ceateTableAllBonus(data)).catch(err => console.log(err))
     sendRequest('POST', '/loadBonusesNoT2', bonuses).then(data => ceateTableNoT2Bonus(data)).catch(err => console.log(err))
     $('#dropDownListPhone').on('change', function() {
