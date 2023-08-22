dff9ddfd7b5ea83b93b95065ebd4b837d1832533
Дмитрий Решетников <reshetnikov.dim.mi@yandex.ru>
2023-08-22 16:38:19 +0600

    saveBarcodeSpark

--- src/main/java/com/myisu_1/isu/service/BarcodeServise.java
+++ src/main/java/com/myisu_1/isu/service/BarcodeServise.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -39,7 +40,6 @@ public class BarcodeServise {
         barcodeSparkRepository.deleteAll();
         try {
             List<BarcodeSpark> barcodeSparkList = new ArrayList<>();
-
             XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
             XSSFSheet worksheet = workbook.getSheetAt(0);
             for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
@@ -58,8 +58,6 @@ public class BarcodeServise {
         DateFormat df = new SimpleDateFormat("HH 'hours', mm 'mins,' ss 'seconds'");
         df.setTimeZone(TimeZone.getTimeZone("GMT+0"));
         return new ResponseEntity<>("File uploaded за" + " - " + df.format(new Date(timeWorkCode)), HttpStatus.OK);
-
-
     }
 
     public ResponseEntity<String> saveBarcodeUnf(MultipartFile file) {
@@ -96,6 +94,7 @@ public class BarcodeServise {
         List<String> barcode = new ArrayList<>();
         try {
             docUnfList = new ArrayList<>();
+
             XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
             XSSFSheet worksheet = workbook.getSheetAt(0);
             for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
@@ -114,7 +113,9 @@ public class BarcodeServise {
             ex.printStackTrace();
             return new ResponseEntity<>("Invalid file format!!", HttpStatus.BAD_REQUEST);
         }
+
         barcode.addAll(docUnfRepository.shkDocUnfs());
+
         docUnfs = docUnfRepository.shkDocUnf(barcode);
 
         return new ResponseEntity<>("Загружено строк"+"  "+ docUnfs.size()+"  "+ "из" + "  "+ docUnfList.size(), HttpStatus.OK);
