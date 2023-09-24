5723783f6a01a011afd812c5aba45082c87edde9
Mr. Dmitry <62257718+reshetnikovdimmi@users.noreply.github.com>
2023-09-24 09:36:34 +0600

    createMatrixT2

--- src/main/java/com/myisu_1/isu/models/Matrix/Matrix.java
+++ src/main/java/com/myisu_1/isu/models/Matrix/Matrix.java
@@ -1,21 +1,36 @@
 package com.myisu_1.isu.models.Matrix;
 
 import com.myisu_1.isu.dto.OrderRecommendations;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Data
+@Getter
+@Setter
 public class Matrix {
     public List<String> distributionModelList;
     public List<OrderRecommendations> remainMatrixList;
+    Matrix dtoMatrix;
+    Map<String,Map<String,Integer>> shopRemains ;
 
-    private String distributionModel;
-    private Map<String,Integer> shopRemains;
 
     public Object createMatrix() {
-List<Matrix> matrixList = new ArrayList<>();
+        shopRemains = new HashMap<>();
+        Map<String,Integer> distributionMode = new HashMap<>();
 
-        return null;
+for (OrderRecommendations o:remainMatrixList){
+    shopRemains.put(o.getShop(),distributionMode);
+}
+
+
+        dtoMatrix = new Matrix();
+        dtoMatrix.setDistributionModelList(distributionModelList);
+        dtoMatrix.setShopRemains(shopRemains);
+        return dtoMatrix;
     }
 }
